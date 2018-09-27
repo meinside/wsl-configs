183b01bc724f5a80084782dedc34c1bd926101ef
Sungjin Han <meinside@gmail.com>
2018-09-27 12:09:43 +0900

    Add bin/install_nvim.sh

---
+++ bin/install_nvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Install NeoVIM from the source,
+#
+# with this script: https://github.com/meinside/rpi-configs/blob/master/bin/install_nvim.sh
+#
+#
+# created on: 2018.09.27.
+#
+# by meinside@gmail.com
+
+cd ~
+wget -O - "https://github.com/meinside/rpi-configs/raw/master/bin/install_nvim.sh" | bash
